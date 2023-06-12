bedd906f5d9d7c377e5669068fce21f3addc803a
fakeboom <fakeboom@foxmail.com>
2023-06-13 00:23:16 +0800

    change api

--- src/data/LimitOrder.ts
+++ src/data/LimitOrder.ts
@@ -37,8 +37,8 @@ export function useLimitOrdersData(){
     useEffect(()=>{
         const queryFunc = async () => {
           if(blockNumber&& blockNumber > 0){
-            //const limitOrders = await(await fetch(APIHost + "/getordersbyuser/" + account)).json();
-            const limitOrders = await(await fetch( "http://43.154.22.163:8080/api/getordersbyuser/" + account)).json();
+            const limitOrders = await(await fetch(APIHost + "/getordersbyuser/" + account)).json();
+            //const limitOrders = await(await fetch( "http://43.154.22.163:8080/api/getordersbyuser/" + account)).json();
             // tododo, 待处理 fix
             if(limitOrders && limitOrders.data){
                 SetData(limitOrders.data)
